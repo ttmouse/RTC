e96a99ad01791fa0345832dff5d95d677ea57c14
ttmouse <ttmouseg@gmail.com>
2026-09-10 10:05:33 +0800

    unify config data dir to com.rtc.transcriber

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const DATA_ROOT = process.env.RTC_DATA_DIR || path.join(
   os.homedir(),
   'Library',
   'Application Support',
-  'rtc-transcriber'
+  'com.rtc.transcriber'
 );
 const EVENTS_DIR = path.join(DATA_ROOT, 'events');
 const CONFIG_PATH = path.join(DATA_ROOT, 'config.json');
